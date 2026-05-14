cf599c409b6de136a6ab1674833eb8e4b5a6ad9f
mukeshvardhanG <gaddipatimukeshvardhan@gmail.com>
2026-05-14 06:39:19 +0530

    I have printed array

---
+++ Array.java
@@ -0,0 +1,53 @@
+import java.lang.*;
+import java.util.*;
+
+class Array
+{
+    public static void main(String[] args) 
+    {
+        // int[] arr; // declaration of array
+
+        // arr = new int[3]; // memory creation for 3 integers
+
+        // int arr[] = {12,1,3,4,5,6,7}; direct initilization
+
+        // int[] arr = new int[3];
+
+        // arr[0] = 12;
+        // arr[1] = 13;
+        // arr[2] = 15;
+
+        // int[] arr = {1,2,3,4,5};
+        // System.out.println(arr.length);
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(Arrays.toString(nums));
+
+
+        // Scanner sc = new Scanner(System.in);
+        // int[] arr = new int[9];
+
+        // System.out.print("Enter array values");
+
+        // for(int i = 0; i < arr.length; i++)
+        // {
+        //     arr[i] = sc.nextInt();
+        // }
+
+        // for(int i = 0; i < arr.length; i++)
+        // {
+        //     System.out.print(arr[i]+" ");
+        // }
+
+        // System.out.println("Or else we can use direct array class");
+
+        // System.out.println(Arrays.toString(arr));
+
+
+        
+
+    }
+}
